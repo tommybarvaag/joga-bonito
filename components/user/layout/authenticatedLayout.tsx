29084665980bb16dca3e3b56db24d65633cb9383
Tommy Barvåg <tommy@barvaag.com>
2022-02-23 10:50:43 +0100

    Add refetch interval to session provider

--- components/user/layout/authenticatedLayout.tsx
+++ components/user/layout/authenticatedLayout.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from "next-auth/react";
 
 const AuthenticatedLayout = ({ children, pageProps }: { children: React.ReactNode; pageProps: any }) => {
   return (
-    <SessionProvider session={pageProps?.session}>
+    <SessionProvider session={pageProps?.session} refetchInterval={30 * 60}>
       <UserProvider user={pageProps?.session?.user}>
         <VoteProvider votes={pageProps?.cageballEvents?.reduce((result, current) => [...result, ...current.votes], [])}>{children}</VoteProvider>
       </UserProvider>
