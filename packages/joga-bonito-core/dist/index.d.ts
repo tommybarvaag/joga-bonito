4f52eca009a6c8cb7d787baa153b6bb91fec5bfa
Tommy Barvåg <tommy@barvaag.com>
2022-03-16 09:17:10 +0100

    Update according to gitignore

--- packages/joga-bonito-core/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-declare const Button: ({ children }: {
-    children?: React.ReactNode;
-}) => JSX.Element;
-
-export { Button };
--- packages/joga-bonito-core/dist/index.mjs
+++
@@ -1,9 +0,0 @@
-// src/button.tsx
-import * as React from "react";
-var Button = ({ children }) => {
-  return /* @__PURE__ */ React.createElement("button", null, children);
-};
-var button_default = Button;
-export {
-  button_default as Button
-};
